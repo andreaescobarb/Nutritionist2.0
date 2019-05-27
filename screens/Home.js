0495c628f0bf9d06173f5613df3df2f2b44e899f
carlos114771 <carlosalfredoriveramilian@yahoo.es>
2019-05-27 17:01:08 -0600

    fixing home

--- screens/Home.js
+++ screens/Home.js
@@ -8,76 +8,22 @@ const { width } = Dimensions.get('screen');
 import products from '../constants/products';
 
 export default class Home extends React.Component{
-  renderSearch = () => {
-    const { navigation } = this.props;
-    const iconCamera = <Icon size={16} color={theme.COLORS.MUTED} name="camera-18" family="GalioExtra" />
-
-    return (
-      <Input
-        right
-        color="black"
-        style={styles.search}
-        iconContent={iconCamera}
-        placeholder="What are you looking for?"
-        onFocus={() => navigation.navigate('Pro')}
-      />
-    )
-  }
   
-  renderTabs = () => {
-    const { navigation } = this.props;
 
-    return (
-      <Block row style={styles.tabs}>
-        <Button shadowless style={[styles.tab, styles.divider]} onPress={() => navigation.navigate('Pro')}>
-          <Block row middle>
-            <Icon name="grid-square" family="Galio" style={{ paddingRight: 8 }} />
-            <Text size={16} style={styles.tabTitle}>Categories</Text>
-          </Block>
-        </Button>
-        <Button shadowless style={styles.tab} onPress={() => navigation.navigate('Pro')}>
-          <Block row middle>
-            <Icon size={16} name="camera-18" family="GalioExtra" style={{ paddingRight: 8 }} />
-            <Text size={16} style={styles.tabTitle}>Best Deals</Text>
-          </Block>
-        </Button>
-      </Block>
-    )
-  }
 
   renderProducts = () => {
     return (
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.products}>
-        <Block flex>
-          <Product product={products[0]} horizontal />
-          <Block flex row>
-            <Product product={products[1]} style={{ marginRight: theme.SIZES.BASE }} />
-            <Product product={products[2]} />
-          </Block>
-          <Product product={products[3]} horizontal />
-          <Product product={products[4]} full />
-        </Block>
+      <ScrollView>
+        
       </ScrollView>
     )
   }
 
   render() {
-    const { navigation } = this.props;
     return (
-      <ScrollView>
-        <Block flex center style={styles.home}>
-          {this.renderProducts()}
-        </Block>
-        <Block center>
-          <Button
-          onPress={() => navigation.navigate('Onboarding')}
-          shadowless color="warning">
-            Sign Out
-          </Button>
-        </Block>
-      </ScrollView>
+      <Block flex center style={styles.home}>
+        {this.renderProducts()}
+      </Block>
     );
   }
 }
