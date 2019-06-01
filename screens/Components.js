cfbefda4ad0e23a9e538930bf577e2b4c9bf5949
Marvin Estrada <marvinestrada2@gmail.com>
2019-05-31 21:53:05 -0600

    Deleting commented code

--- screens/Components.js
+++ screens/Components.js
@@ -60,35 +60,6 @@ export default class Components extends React.Component {
   renderItem = ({ item }) => {
     const { navigate } = this.props.navigation;
   }
-  /*
-      switch (item.type) {
-        case 'switch':
-          return (
-            <Block row middle space="between" style={styles.rows}>
-              <Text size={14}>{item.title}</Text>
-              <Switch
-                onValueChange={() => this.toggleSwitch(item.id)}
-                ios_backgroundColor={materialTheme.COLORS.SWITCH_OFF}
-                thumbColor={Platform.OS === 'android' ? materialTheme.COLORS.SWITCH_OFF : null}
-                trackColor={{ false: materialTheme.COLORS.SWITCH_OFF, true: materialTheme.COLORS.SWITCH_ON }}
-                value={this.state[item.id]}
-              />
-            </Block>
-          );
-        case 'button':
-          return (
-            <Block style={styles.rows}>
-              <TouchableOpacity onPress={() => navigate('Pro')}>
-                <Block row middle space="between" style={{ paddingTop: 7 }}>
-                  <Text size={14}>{item.title}</Text>
-                  <Icon name="stre-right" family="Galio" style={{ paddingRight: 5 }} />
-                </Block>
-              </TouchableOpacity>
-            </Block>);
-        default:
-          break;
-      }
-    }*/
 
   render() {
     const { navigation } = this.props;
@@ -98,9 +69,7 @@ export default class Components extends React.Component {
           style={styles.components}
           showsVerticalScrollIndicator={false}
         >
-          {/*{this.renderButtons()}*/}
           <Block flex>
-            {/*<Text bold size={16} style={styles.title}>Buttons</Text>*/}
             <Text>{"\n"}</Text>
             <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
               <Block center>
