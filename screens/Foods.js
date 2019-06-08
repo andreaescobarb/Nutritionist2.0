6a147429a6fd1f2d465f98b9f59c188a5246cf54
carlos114771 <alfreodoriverac25@gmail.com>
2019-06-08 00:02:40 -0600

    adding a tag opcion in foods screen

--- screens/Foods.js
+++ screens/Foods.js
@@ -1,157 +1,207 @@
-import React from 'react';
-import { StyleSheet, Dimensions, ScrollView, KeyboardAvoidingView } from 'react-native';
-import { Button, Block, Text, Input, theme } from 'galio-framework';
-import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
-import { AppRegistry, View, Image, TouchableOpacity } from 'react-native';
-const { width } = Dimensions.get('screen');
-import { materialTheme } from '../constants';
+import React from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  KeyboardAvoidingView
+} from "react-native";
+import { Button, Block, Text, Input, theme } from "galio-framework";
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize
+} from "react-native-responsive-dimensions";
+import {
+  AppRegistry,
+  View,
+  Image,
+  TouchableOpacity,
+  Picker
+} from "react-native";
+const { width } = Dimensions.get("screen");
+import { materialTheme } from "../constants";
 import Tags from "react-native-tags";
-import { Card} from 'react-native-elements';
+import { Card } from "react-native-elements";
 
-
+let parameters = {
+  description: ""
+};
 
 export default class Foods extends React.Component {
-    state = {
-        foods: [],
-        tagMap: {} 
-    };
-     
-    renderForm = () => {
-        const { navigation } = this.props;
-        return (
-            <Block flex style={styles.group}>
-                {this.renderFoods(this.state.foods)}
-            </Block> 
-        )
-    }
+  state = {
+    foods: [],
+    tagMap: {}
+  };
 
-    renderFoods = (foods) => {
-        return foods.map((food) => {
-            return(
-                <Card 
-                  title={food.name}
-                  image={{uri: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fbuying-healthy-foods-ft-blog0617.jpg'}}>
-                  <Text style={{marginBottom: 5}}>
-                    {food.description}
-                  </Text>
-                   <Tags
-                        initialTags={this.state.tagMap[food.id]}
-                        renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
-                        <TouchableOpacity key={`${tag}-${index}`} onPress={onPress}>
-                            <Text>{tag}</Text>
-                          </TouchableOpacity>
-                        )}
-                        />
-                </Card>)
-        })
-    }
+  renderForm = () => {
+    const { navigation } = this.props;
+    return (
+      <Block flex style={styles.group}>
+        {this.renderFoods(this.state.foods)}
 
-    render() {
-        return (
-            <Block flex center >
-                <ScrollView
-                    style={styles.components}
-                    showsVerticalScrollIndicator={false}>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Picker
+            selectedValue={parameters.description}
+            onVaueChange={(itemValue, itemIndex) =>
+              (parameters.description = itemValue)
+            }
+          >
+            <Picker.Item label="Desayuno" value="desayuno" />
+            <Picker.Item label="Almuerzo" value="almuerzo" />
+            <Picker.Item label="Cena" value="cena" />
+          </Picker>
+        </Block>
+      </Block>
+    );
+  };
 
-                    {this.renderForm()}
-                </ScrollView>
-            </Block>
-        );
-    }
+  renderFoods = foods => {
+    return foods.map(food => {
+      return (
+        <Card
+          title={food.name}
+          image={{
+            uri:
+              "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fbuying-healthy-foods-ft-blog0617.jpg"
+          }}
+        >
+          <Text style={{ marginBottom: 5 }}>{food.description}</Text>
+          <Tags
+            initialTags={this.state.tagMap[food.id]}
+            renderTag={({
+              tag,
+              index,
+              onPress,
+              deleteTagOnPress,
+              readonly
+            }) => (
+              <TouchableOpacity key={`${tag}-${index}`} onPress={onPress}>
+                <Text>{tag}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </Card>
+      );
+    });
+  };
 
-    componentDidMount(){
-        fetch('https://nutrionist-server.herokuapp.com/foods', {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-            },
-        }).then((response) => response.json())
-            .then((responseJson) => {
+  renderButton = () => {
+    return (
+      <Block flex>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Block center>
+            <Button shadowless style={[styles.button, styles.shadow]}>
+              Agregar Etiqueta
+            </Button>
+          </Block>
+        </Block>
+      </Block>
+    );
+  };
 
-                let tagMap = {};
+  render() {
+    return (
+      <Block flex center>
+        <ScrollView
+          style={styles.components}
+          showsVerticalScrollIndicator={false}
+        >
+          {this.renderForm()}
+        </ScrollView>
+      </Block>
+    );
+  }
 
-                if(!responseJson){
-                    responseJson = [];
-                }
+  componentDidMount() {
+    fetch("https://nutrionist-server.herokuapp.com/foods", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        let tagMap = {};
 
-                responseJson.forEach(function(item){
-                    tagMap[item.id] = item.tags.map(function(tag){return tag.name + " "});
-                });
+        if (!responseJson) {
+          responseJson = [];
+        }
 
+        responseJson.forEach(function(item) {
+          tagMap[item.id] = item.tags.map(function(tag) {
+            return tag.name + " ";
+          });
+        });
 
-                this.setState({foods:responseJson}) ;
-                this.setState({tagMap:tagMap}) ;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+        this.setState({ foods: responseJson });
+        this.setState({ tagMap: tagMap });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 }
 
-
 const styles = StyleSheet.create({
-    components: {
+  components: {},
+  title: {
+    paddingVertical: theme.SIZES.BASE,
+    paddingHorizontal: theme.SIZES.BASE * 2
+  },
+  group: {
+    paddingTop: 20
+  },
+  shadow: {
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    shadowOpacity: 0.2,
+    elevation: 2
+  },
+  button: {
+    marginBottom: theme.SIZES.BASE,
+    width: width - theme.SIZES.BASE * 2
+  },
+  optionsText: {
+    fontSize: theme.SIZES.BASE * 0.75,
+    color: "#4A4A4A",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: -0.29
+  },
+  optionsButton: {
+    width: "auto",
+    height: 34,
+    paddingHorizontal: theme.SIZES.BASE,
+    paddingVertical: 10
+  },
+  input: {
+    borderBottomWidth: 1
+  },
 
-    },
-    title: {
-        paddingVertical: theme.SIZES.BASE,
-        paddingHorizontal: theme.SIZES.BASE * 2,
-    },
-    group: {
-        paddingTop: 20
-    },
-    shadow: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 4,
-        shadowOpacity: 0.2,
-        elevation: 2,
-    },
-    button: {
-        marginBottom: theme.SIZES.BASE,
-        width: width - (theme.SIZES.BASE * 2),
-    },
-    optionsText: {
-        fontSize: theme.SIZES.BASE * 0.75,
-        color: '#4A4A4A',
-        fontWeight: "normal",
-        fontStyle: "normal",
-        letterSpacing: -0.29,
-    },
-    optionsButton: {
-        width: 'auto',
-        height: 34,
-        paddingHorizontal: theme.SIZES.BASE,
-        paddingVertical: 10,
-    },
-    input: {
-        borderBottomWidth: 1,
-    },
+  inputDefault: {
+    borderBottomColor: materialTheme.COLORS.PLACEHOLDER
+  },
+  inputTheme: {
+    borderBottomColor: materialTheme.COLORS.PRIMARY
+  },
+  inputTheme: {
+    borderBottomColor: materialTheme.COLORS.PRIMARY
+  },
+  inputInfo: {
+    borderBottomColor: materialTheme.COLORS.INFO
+  },
+  inputSuccess: {
+    borderBottomColor: materialTheme.COLORS.SUCCESS
+  },
+  inputWarning: {
+    borderBottomColor: materialTheme.COLORS.WARNING
+  },
+  inputDanger: {
+    borderBottomColor: materialTheme.COLORS.ERROR
+  },
 
-    inputDefault: {
-        borderBottomColor: materialTheme.COLORS.PLACEHOLDER,
-    },
-    inputTheme: {
-        borderBottomColor: materialTheme.COLORS.PRIMARY,
-    },
-    inputTheme: {
-        borderBottomColor: materialTheme.COLORS.PRIMARY,
-    },
-    inputInfo: {
-        borderBottomColor: materialTheme.COLORS.INFO,
-    },
-    inputSuccess: {
-        borderBottomColor: materialTheme.COLORS.SUCCESS,
-    },
-    inputWarning: {
-        borderBottomColor: materialTheme.COLORS.WARNING,
-    },
-    inputDanger: {
-        borderBottomColor: materialTheme.COLORS.ERROR,
-    },
-
-    rows: {
-        height: theme.SIZES.BASE * 2,
-    },
-
-})
+  rows: {
+    height: theme.SIZES.BASE * 2
+  }
+});
