aaa56bceb241055abaf2665de38151ab00c3ec80
MichelleLopez <michelle.lm@unitec.edu>
2019-06-11 01:38:33 -0600

    ImagePicker added to AddFood and EditFood

--- screens/EditFood.js
+++ screens/EditFood.js
@@ -1,76 +1,185 @@
 import React from 'react';
+import axios from 'axios';
 
-import { StyleSheet, Alert, Dimensions, ScrollView, KeyboardAvoidingView , AsyncStorage} from 'react-native';
+import { Image, View } from 'react-native';
+import { ImagePicker, Permissions, Constants} from 'expo';
+import { StyleSheet, Dimensions, ScrollView, Platform, KeyboardAvoidingView, AsyncStorage } from 'react-native';
 import { Button, Block, Text, Input, theme } from 'galio-framework';
-import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
 
 const { width } = Dimensions.get('screen');
 import { materialTheme } from '../constants';
 
-
-
 let parameters = {
     name: '',
-    description: ''
+    description: '',
+    image: ''
 };
 
-export default class AddFood extends React.Component {
-    renderForm = () => {
-        const { navigation } = this.props;
-        return (
-            <Block flex style={styles.group}>
-                <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
-                    <Text>Nombre de Comida</Text>
+let addFood = async ()  =>{
+    axios.post('https://nutrionist-server.herokuapp.com/foods', parameters).then(async function(response) {
+        let data = response.data;
+        if (data.created) {
+            Alert.alert(
+                'Error al crear comida'
+            )
+        } else {
+            Alert.alert(
+                'Nueva comida creada...'
+            )
+            const value = await AsyncStorage.setItem('food',JSON.stringify(foods));
+            navigation.navigation('login')
+        }
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+
+export default class AddFood extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            name:'',
+            nameValdate:true,
+            description:'',
+            descriptionValdate:true,
+            image: '',
+        }
+    }
+    state = {
+        imagePicked: null,
+      };
+
+    validate(text,type){
+        namevalidation=/^[a-zA-Z]+$/
+        descriptionvalidation=/^[a-zA-Z]+$/
+        if(type=='name'){
+            if(namevalidation.test(text)){
+                this.setState({
+                    nameValdate:true,
+                })
+                console.warn("text is correct")
+            }else{
+                this.setState({
+                    nameValdate:false,
+                })
+                console.warn("invalid text")
+            }
+        }else  if(type=='description'){
+            if(descriptionvalidation.test(text)){
+                this.setState({
+                    descriptionValdate:true,
+                })
+                console.warn("text is correct")
+            }else{
+                this.setState({
+                    descriptionValdate:false,
+                })
+                console.warn("invalid text")
+            }
+        }
+    } 
+
+    renderForm=()=>{
+        const {navigation}= this.props;
+        let { imagePicked } = this.state;
+        return(
+            <KeyboardAvoidingView>
+            <Block flex style ={styles.group}>
+                <Block style ={{paddingHorizontal: theme.SIZES.BASE}}>
+                    <Text h7 style ={{marginBottom: theme.SIZES.BASE/2}}>Nombre</Text>
                 </Block>
-                <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
-                    <Input right placeholder="Ingresar nuevo nombre"
-                        placeholderTextColor={materialTheme.COLORS.DEFAULT}
-                        style={{ boderRadius: 3, borderColor: materialTheme.COLORS.INPUT }}
-                        onChangeText={(value) => parameters.name =value}   
+                <Block style={{paddingHorizontal: theme.SIZES.BASE}}>
+                    <Input right placeholder="Ingrese Nombre de Comida" 
+                        color={materialTheme.COLORS.ICON}
+                        placeholderTextColor= {materialTheme.COLORS.DEFAULT}
+                        onChangeText={(value) => parameters.name =value}
+//                        onChangeText={(text) => this.validate(text,"name")}   
+                        style={[{boderRadius: 3, borderColor: materialTheme.COLORS.INPUT},!this.state.nameValdate?styles.error:null]}
                     />
                 </Block>
 
-                <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
-                    <Text  style={{ marginBottom: theme.SIZES.BASE / 2 }}>Descripción</Text>
+                <Block style ={{paddingHorizontal: theme.SIZES.BASE}}>
+                    <Text h7 style ={{marginBottom: theme.SIZES.BASE/2}}>Descripción</Text>
                 </Block>
-                <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
-                    <Input right placeholder="Ingresar nueva descripción"
-                        placeholderTextColor={materialTheme.COLORS.DEFAULT}
-                        onChangeText={(value) => parameters.description =value}                        s
-                        style={{ boderRadius: 3, borderColor: materialTheme.COLORS.INPUT }}
+                <Block style={{paddingHorizontal: theme.SIZES.BASE}}>
+                    <Input right placeholder="Ingrese Descripción de comida" 
+                        color={materialTheme.COLORS.ICON}
+                        placeholderTextColor= {materialTheme.COLORS.DEFAULT}
+                        onChangeText={(value) => parameters.description =value}
+//                        onChangeText={(text) => this.validate(text,"description")}   
+                        style={[{boderRadius: 3, borderColor: materialTheme.COLORS.INPUT},!this.state.descriptionValdate?styles.error:null]}
                     />
                 </Block>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Button shadowless style={[styles.button, styles.shadow]} 
+                    onPress={this._pickImage}>
+                        Selecciona una imagen
+                    </Button>
+                    
+                    {imagePicked &&
+                    <Image source={{ uri: imagePicked }} style={{ width: 200, height: 200 }} />}
+                </View>
             </Block>
+         </KeyboardAvoidingView>
         )
-
     }
+    componentDidMount() {
+        this.getPermissionAsync();
+      }
+    
+      getPermissionAsync = async () => {
+        if (Constants.platform.ios) {
+          const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+          if (status !== 'granted') {
+            alert('Sorry, we need camera roll permissions to make this work!');
+          }
+        }
+      }
+    
+      _pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          base64: true,
+          allowsEditing: true,
+          aspect: [4, 3],
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+          this.setState({ imagePicked: result.uri});
+          parameters.image = result.uri;
+        }
+      };
 
-    renderButton = () => {
+    renderButton = () =>{
         const { navigation } = this.props;
-        return (
-            <Block flex>
-                <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
-                    <Block center>
-                        <Button 
-                            shadowless style={[styles.button, styles.shadow]}>
-                            Actualizar
-                        </Button>
-                    </Block>
+        return(
+        <Block flex>
+            <Block style ={{paddingHorizontal: theme.SIZES.BASE}}>
+                <Block center>
+                    <Button 
+                    shadowless style={[styles.button, styles.shadow]} 
+                    onPress={() =>addFood(navigation)}>
+                        Crear Comida
+                    </Button>
                 </Block>
             </Block>
-        )
+        </Block>
+       )
     }
 
-    render() {
-        return (
+    render(){
+        return(
             <Block flex center >
                 <ScrollView
                     style={styles.components}
                     showsVerticalScrollIndicator={false}>
 
-                    {this.renderForm()}
-                    {this.renderButton()}
-                </ScrollView>
+                        {this.renderForm()}
+                        {this.renderButton()}
+                    </ScrollView>
             </Block>
         );
     }
@@ -78,19 +187,20 @@ export default class AddFood extends React.Component {
 
 
 const styles = StyleSheet.create({
-    components: {
+    components:{
 
     },
-    title: {
+    title:{
         paddingVertical: theme.SIZES.BASE,
-        paddingHorizontal: theme.SIZES.BASE * 2,
+        paddingHorizontal: theme.SIZES.BASE *2,
     },
-    group: {
+    group:{
         paddingTop: theme.SIZES.BASE * 3.75,
+        color: 'black'
     },
-    shadow: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
+    shadow:{
+        shadowColor:'black',
+        shadowOffset:{width:0,height:2},
         shadowRadius: 4,
         shadowOpacity: 0.2,
         elevation: 2,
@@ -98,50 +208,52 @@ const styles = StyleSheet.create({
     button: {
         marginBottom: theme.SIZES.BASE,
         width: width - (theme.SIZES.BASE * 2),
-    },
-    optionsText: {
+      },
+      optionsText: {
         fontSize: theme.SIZES.BASE * 0.75,
         color: '#4A4A4A',
         fontWeight: "normal",
         fontStyle: "normal",
         letterSpacing: -0.29,
-    },
-    optionsButton: {
+      },
+      optionsButton: {
         width: 'auto',
         height: 34,
         paddingHorizontal: theme.SIZES.BASE,
         paddingVertical: 10,
-    },
-    input: {
-        borderBottomWidth: 1,
-    },
-
-    inputDefault: {
-        borderBottomColor: materialTheme.COLORS.PLACEHOLDER,
-    },
-    inputTheme: {
-        borderBottomColor: materialTheme.COLORS.PRIMARY,
-    },
-    inputTheme: {
-        borderBottomColor: materialTheme.COLORS.PRIMARY,
-    },
-    inputInfo: {
-        borderBottomColor: materialTheme.COLORS.INFO,
-    },
-    inputSuccess: {
-        borderBottomColor: materialTheme.COLORS.SUCCESS,
-    },
-    inputWarning: {
-        borderBottomColor: materialTheme.COLORS.WARNING,
-    },
-    inputDanger: {
-        borderBottomColor: materialTheme.COLORS.ERROR,
-    },
-
-    rows: {
-        height: theme.SIZES.BASE * 2,
-    },
-    Text:{
-        fontSize: responsiveFontSize(2)
       },
+      input: {
+        borderBottomWidth: 1,
+      },
+
+      inputDefault: {
+        borderBottomColor: materialTheme.COLORS.PLACEHOLDER,
+      },
+      inputTheme: {
+        borderBottomColor: materialTheme.COLORS.PRIMARY,
+      },
+      inputTheme: {
+        borderBottomColor: materialTheme.COLORS.PRIMARY,
+      },
+      inputInfo: {
+        borderBottomColor: materialTheme.COLORS.INFO,
+      },
+      inputSuccess: {
+        borderBottomColor: materialTheme.COLORS.SUCCESS,
+      },
+      inputWarning: {
+        borderBottomColor: materialTheme.COLORS.WARNING,
+      },
+      inputDanger: {
+        borderBottomColor: materialTheme.COLORS.ERROR,
+      },
+     
+      rows: {
+        height: theme.SIZES.BASE * 2,
+      },
+      error:{
+        borderWidth:2,
+        borderColor:'red'
+    }
+
 })
